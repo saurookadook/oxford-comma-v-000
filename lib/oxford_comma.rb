3dddfd05451210cc18e71bfdb991a3303c1d8bf5
Andy Maskiell <maskiella@gmail.com>
2017-12-03 19:40:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,11 @@
 def oxford_comma(array)
   list = []
-  array.join
+  array.each do |item|
+    if item.last == false
+      "#{item}, " << list
+    elsif item.last
+      " and #{item}" << list
+    end
+  end
+  list.join
 end
