4e85e541521d32989569ae4ecce2c271d5d78abe
Andy Maskiell <maskiella@gmail.com>
2017-12-03 20:28:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    list = []
+    array.each do |item|
+      if item != array.at(-1)
+        list << "#{item}, "
+      elsif array.at(-1)
+        list << "and #{item}"
+      end
+    end
+    list.join
+  end
 end
