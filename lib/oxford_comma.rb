d05a9c5e5d78357a0b88a56d90ac75e3a263436a
Andy Maskiell <maskiella@gmail.com>
2017-12-03 19:55:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,19 @@
 def oxford_comma(array)
-  list = []
-  array.each do |item|
-    if array.last == false
-      "#{item}, " << list
-    elsif array.last
-      " and #{item}" << list
-    end
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
   end
-  list.join
+    
+
+  # list = []
+  # array.each do |item|
+  #   if array.last == false
+  #     "#{item}, " << list
+  #   elsif array.last
+  #     " and #{item}" << list
+  #   end
+  # end
+  # list.join
 end
