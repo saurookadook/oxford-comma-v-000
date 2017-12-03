19dc00917d17a71acd05bd03aa9bbfd36d7688c7
Andy Maskiell <maskiella@gmail.com>
2017-12-03 19:49:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
-  # list = []
-  # array.each do |item|
-  #   if array.last == false
-  #     "#{item}, " << list
-  #   elsif array.last
-  #     " and #{item}" << list
-  #   end
-  # end
-  # list.join
+  list = []
+  array.each do |item|
+    if array.last == false
+      "#{item}, " << list
+    elsif array.last
+      " and #{item}" << list
+    end
+  end
+  list.join
 end
